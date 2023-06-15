1c08ff447527a0b7cd8afa2e105e4cdc72f860c6
Joshua Dishong <dishong.joshua@yahoo.com>
2023-06-15 17:53:39 -0400

    resubmit to new repository

---
+++ MathOperations/MathOperations/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace MathOperations
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Addition
+            //int num1 = 3;
+            //int num2 = 5;
+            //int sum = num1 + num2;
+            //Console.WriteLine("3 + 5 is " + sum);
+            //Console.ReadLine();
+
+            //Subtraction
+            //int num1 = 5;
+            //int num2 = 3;
+            //int difference = num1 - num2;
+            //Console.WriteLine("5 - 3 is " + difference);
+            //Console.ReadLine();
+
+            //Multiplication
+            //int num1 = 3;
+            //int num2 = 5;
+            //int product = num1 * num2;
+            //Console.WriteLine("3 times 5 is " + product);
+            //Console.ReadLine();
+
+            // Division
+            int num1 = 15;
+            int num2 = 3;
+            int quotient = num1 / num2;
+            Console.WriteLine("15 divided by 3 is " + quotient);
+            Console.ReadLine();
+        }
+    }
+}
