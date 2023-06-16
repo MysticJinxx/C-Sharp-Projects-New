9b5f98b405461791a9366b57ccdcd48c8676a7a7
Joshua Dishong <dishong.joshua@yahoo.com>
2023-06-16 13:35:39 -0400

    math challenge and income comparison assignment

---
+++ AnonymousIncomeComparison/AnonymousIncomeComparison/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AnonymousIncomeComparison
+{
+    // Program takes 2 entered persons, prints their annual salary, and shows if person 1 makes more money than person 2
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Anonymous Income Comparison Program");
+            // Person 1 details inputed here
+            Console.WriteLine("Person 1");
+            Console.WriteLine("What is their hourly rate?");
+            int hourlyRateOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many hours are worked each week?");
+            int hoursPerWeekOne = Convert.ToInt32(Console.ReadLine());
+
+            // Person 2 details inputed here
+            Console.WriteLine("Person 2");
+            Console.WriteLine("What is their hourly rate?");
+            int hourlyRateTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many hours are worked each week?");
+            int hoursPerWeekTwo = Convert.ToInt32(Console.ReadLine());
+
+            // Person 1's Annual Salary
+            Console.WriteLine("Annual salary of Person 1:");
+            int personOneSalary = (hourlyRateOne * hoursPerWeekOne) * 52;
+            Console.WriteLine(personOneSalary);
+      
+            // Person 2's Annual Salary
+            Console.WriteLine("Annual salary of Person 2:");
+            int personTwoSalary = (hourlyRateTwo * hoursPerWeekTwo) * 52;
+            Console.WriteLine(personTwoSalary);
+         
+            // Comparison
+            Console.WriteLine("Does Person 1 make more money than Person 2?");
+            bool isMore = personOneSalary > personTwoSalary;
+            Console.WriteLine(isMore);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleAppChallenge/ConsoleAppChallenge/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace ConsoleAppChallenge
+{
+    // Program to do some math operations
+    class Program
+    {
+        static void Main()
+        {
+            // Takes an entered number and multiplies it by 50
+            Console.WriteLine("Enter an integer");
+            int multNum = Convert.ToInt32(Console.ReadLine());
+            int product = multNum * 50;
+            Console.WriteLine(multNum + " times 50 is " + product);
+            Console.ReadLine();
+
+            // Takes an entered number and adds 25 to it
+            Console.WriteLine("Enter a number");
+            double addNum = Convert.ToDouble(Console.ReadLine());
+            double sum = addNum + 25;
+            Console.WriteLine(addNum + " plus 25 is " + sum);
+            Console.ReadLine();
+
+            // Takes an entered number and divides it by 12.5
+            Console.WriteLine("Enter a number");
+            double divNum = Convert.ToDouble(Console.ReadLine());
+            double quotient = divNum / 12.5;
+            Console.WriteLine(divNum + " divided by 12.5 is " + quotient);
+            Console.Read();
+
+            // Takes an entered number and compares it to 50
+            Console.WriteLine("Enter a number");
+            int boolNum = Convert.ToInt32(Console.ReadLine());
+            bool greaterThan = boolNum > 50;
+            Console.WriteLine(greaterThan);
+            Console.ReadLine();
+
+            // Takes an entered number and finds the remainder if divided by 7
+            Console.WriteLine("Enter a number");
+            int modNum = Convert.ToInt32(Console.ReadLine());
+            int remainder = modNum % 7;
+            Console.WriteLine(modNum + " divdided by 7, has a remainder of " + remainder);
+            Console.ReadLine();
+        }
+    }
+}
