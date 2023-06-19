070b8903fadc9862d1d5c08144b864e9823e2ee4
Joshua Dishong <dishong.joshua@yahoo.com>
2023-06-19 10:52:19 -0400

    added Boolean Logic and CarInsurance Program

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // AND Operator
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            // OR Operator
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+
+            // Equals Operator
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+
+            // Does Not Equal Operator
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+
+            // XOR Operator
+            Console.WriteLine(true ^ true);
+            Console.WriteLine(true ^ false);
+            Console.WriteLine(false ^ false);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BooleanLogicCarInsurance/BooleanLogicCarInsurance/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace BooleanLogicCarInsurance
+{
+    // Program which determines if the user is qualifed for car insurance.
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Car Insurance Qualification Program");
+            // AGE
+            Console.WriteLine("What is your age?");
+            int ageNum = Convert.ToInt32(Console.ReadLine());
+
+            // DUI
+            Console.WriteLine("Have you ever had a DUI? (Please enter 'true' or 'false')");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+
+            // Speeding Tickets
+            Console.WriteLine("How many speeding tickets have you recieved?");
+            int ticNum = Convert.ToInt32(Console.ReadLine());
+
+            // Qualification Results
+            Console.WriteLine("Are you qualified?");
+            Console.WriteLine(ageNum >= 15 && dui == false && ticNum <= 3);
+            Console.ReadLine();
+        }
+    }
+}
