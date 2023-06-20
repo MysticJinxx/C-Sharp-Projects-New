62e0729ae722a84c1b1e6936dd060c4b834513eb
Joshua Dishong <dishong.joshua@yahoo.com>
2023-06-20 11:22:52 -0400

    added loops, updated shipping quote

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 12;
+
+            // adding the 'do loop' forces the switch to run at least once
+            // in the off chance the user guesses correctly the first time
+            do
+            {
+                // switch
+                switch (number)
+                {
+                    case 62:
+                        Console.WriteLine("You guessed 62, try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("You guessed 29, try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed 55, try again.");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 12:
+                        Console.WriteLine("You guessed 12, that is correct!");
+                        isGuessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong!");
+                        Console.WriteLine("Guess a number?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isGuessed); // !isGuessed is the same as isGuessed == false
+           
+           
+            Console.ReadLine();
+        }
+    }
+}
