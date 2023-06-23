c2e0067bde8a858c24c79b40b8753729f10785ab
Joshua Dishong <dishong.joshua@yahoo.com>
2023-06-23 15:35:53 -0400

    added ExceptionHandling

--- ExceptionHandling/ExceptionHandling/Program.cs
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -8,8 +8,44 @@ namespace ExceptionHandling
 {
     class Program
     {
+        // Program that takes two user inputed numbers and divides the first one by the second one.
+        // Try catch block with exceptions
         static void Main(string[] args)
         {
+            try
+            {
+                // User Picks a number
+                Console.WriteLine("Pick a number.");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                // User picks a second number
+                Console.WriteLine("Pick a second number.");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                // Divides first number by second number
+                Console.WriteLine("Dividing the two numbers....");
+                int num3 = num1 / num2;
+                Console.WriteLine(num1 + " divided by " + num2 + " equals " + num3);
+                
+            }
+            catch (FormatException ex) // format exceptions saved in variable ex.
+            {
+                Console.WriteLine("Please type a whole number."); // Displays the error message to screen
+                return;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("error: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+            
         }
     }
 }
