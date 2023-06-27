663dd674bd433b29b69ae2291c79cd5598d41bc6
Joshua Dishong <dishong.joshua@yahoo.com>
2023-06-27 14:01:13 -0400

    Added multiple parameter method

---
+++ MultipleParameterMethod/MultipleParameterMethod/Operations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleParameterMethod
+{
+    public class Operations
+    {
+        public int multMethod (int paraOne, int paraTwo)
+        {
+            int product = paraOne * paraTwo;
+            return product;
+        }
+    }
+}
---
+++ MultipleParameterMethod/MultipleParameterMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleParameterMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations operations = new Operations();
+
+            Console.WriteLine("This program will take an entered number and a second optional number, \nand multiply them together.");
+            Console.WriteLine("Please enter a number.");
+            int userNumOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("OPTIONAL: please enter a second number.");
+            string userString = Console.ReadLine();
+
+            Int32.TryParse(userString, out int userNumTwo);
+
+
+            int answer = operations.multMethod(userNumOne, userNumTwo);
+
+            Console.WriteLine(userNumOne + " times " + userNumTwo + " equals " + answer);
+            Console.ReadLine();
+        }
+    }
+}
