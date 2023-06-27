0b59ecc882e6f821fe5b2fcfe6b9f36b5e43b980
Joshua Dishong <dishong.joshua@yahoo.com>
2023-06-27 13:29:49 -0400

    added Method Overloading

---
+++ MethodOverloading/MethodOverloading/Operations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    public class Operations
+    {
+        // Takes a floating point number and subtracts 125
+        public float opMethod(float subtraction)
+        {
+            float difference = subtraction - 125;
+            return difference;
+        }
+
+        // Takes an intiger and multiplies it by 5
+        public int opMethod(int multiplication)
+        {
+            int product = multiplication * 5;
+            return product;
+        }
+
+        // Takes a string, converts it to a intiger and then adds it, if possible
+        public string opMethod(string addition)
+        {
+            string stringSum = addition + 1000;
+            int sum = Convert.ToInt32(stringSum);
+            return stringSum;
+        }
+
+        //public string opMethod(int )
+        
+
+    }
+}
---
+++ MethodOverloading/MethodOverloading/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations operations = new Operations();
+
+            // Method overload one
+            float answerOne = operations.opMethod(500f);
+            Console.WriteLine("500 minus 125 is equal to " + answerOne);
+         
+
+            // Method overload two
+            int answerTwo = operations.opMethod(478);
+            Console.WriteLine("478 multiplied by 5 is equal to " + answerTwo);
+           
+
+            // Method overload three
+            string answerThree = operations.opMethod("5");
+            Console.WriteLine("\"5\" plus 1000 is equal to " + answerThree);
+            Console.ReadLine();
+        }
+    }
+}
