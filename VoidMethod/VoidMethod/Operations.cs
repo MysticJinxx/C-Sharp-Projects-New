12b38e9729d7b004592e61f980f59d7ff9e1baf9
Joshua Dishong <dishong.joshua@yahoo.com>
2023-06-27 17:08:58 -0400

    added void method

---
+++ VoidMethod/VoidMethod/Operations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Operations
+    {
+        public void voidMethod(int paraOne, int paraTwo)
+        {
+            int answer = paraOne * 10;
+
+            Console.WriteLine(answer);
+            Console.WriteLine(paraTwo);
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations operations = new Operations();
+
+            Console.WriteLine("Enter a number to be multiplied by 10.");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number to be displayed.");
+            int numTwo = Convert.ToInt32(Console.ReadLine()); 
+
+            
+
+            operations.voidMethod(paraOne: numOne, paraTwo: numTwo);
+            Console.ReadLine();
+        }
+    }
+}
