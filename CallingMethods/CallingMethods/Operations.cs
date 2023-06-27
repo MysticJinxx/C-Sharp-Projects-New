fb5c443114611c47e39b8c32289170e912f4aff4
Joshua Dishong <dishong.joshua@yahoo.com>
2023-06-27 12:36:32 -0400

    added calling methods
    quick program that takes a user number and does some math operations on it. Used for learning about classes and methods

---
+++ CallingMethods/CallingMethods/Operations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Operations
+    {
+        public int SumMethod(int addition)
+        {
+            int sum = addition + 50;
+            return sum;
+        }
+
+        public int ProdMethod(int multiplication)
+        {
+            int product = multiplication * 8;
+            return product;
+        }
+
+        public int QuotMethod(int division)
+        {
+            int quotient = division / 2;
+            return quotient;
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations operations = new Operations();
+
+            Console.WriteLine("Please enter a number you want 50 added to. \nYour original number will then be multiplied by 8, \nFinally your original number will be divided by two");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            int answerOne = operations.SumMethod(userNum);
+            int answerTwo = operations.ProdMethod(userNum);
+            int answerThree = operations.QuotMethod(userNum);
+            Console.WriteLine(userNum + " plus 50 equals: " + "\n" + answerOne);
+            Console.WriteLine(userNum + " multiplied by 8 equals: " + "\n" + answerTwo);
+            Console.WriteLine(userNum + " divided by 2 equals: " + "\n" + answerThree);
+            Console.ReadLine();
+        }
+    }
+}
