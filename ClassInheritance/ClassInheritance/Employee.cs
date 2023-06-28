51dfedec23cac7aee58f22510f6933143d61be23
Joshua Dishong <dishong.joshua@yahoo.com>
2023-06-28 11:14:33 -0400

    added ClassInheritance
    little program to learn and practice class inheritance

---
+++ ClassInheritance/ClassInheritance/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInheritance
+{
+    class Employee : Person // Employee inherits from the Person class
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ ClassInheritance/ClassInheritance/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInheritance
+{
+    class Person
+    {
+        // Properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Method which writes a persons full name to the console
+        // format - Name: [full name]
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + LastName);
+        }
+    }
+}
