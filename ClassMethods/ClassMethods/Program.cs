9e79b40271cec95c9f11001b1a10b4104843c83b
Joshua Dishong <dishong.joshua@yahoo.com>
2023-06-28 09:03:34 -0400

    Update Program.cs

--- ClassMethods/ClassMethods/Program.cs
+++ ClassMethods/ClassMethods/Program.cs
@@ -13,18 +13,20 @@ namespace ClassMethods
             // Instantiate class
             Operation operation = new Operation();
 
-            //// Method 1
-            //// User input
-            //Console.WriteLine("Please enter a number that will be divided by 2.");
-            //int userNum = int.Parse(Console.ReadLine());
+            // Method 1
+            // User input
+            Console.WriteLine("Please enter a number that will be divided by 2.");
+            // int.Parse is a way to replace Convert.ToInt32
+            int userNum = int.Parse(Console.ReadLine());
 
-            //operation.outMethod(userNum);
-            //Console.ReadLine();
+            operation.outMethod(userNum);
+            Console.ReadLine();
 
             // Method with output parameters
             // User input
             Console.WriteLine("Please enter a number that will be multiplied by 10, then divided by 2");
             string userString = Console.ReadLine();
+            // Int32.TryParse is a way to replace Convert.ToInt32
             Int32.TryParse(userString, out int userNum2);
 
             operation.outMethod(userNum2, out int result1, out int result2);
