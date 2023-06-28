71d90794d25105297f1c5e2cb5cd2ef0573eaa8d
Joshua Dishong <dishong.joshua@yahoo.com>
2023-06-28 09:00:53 -0400

    added ClassMethods

---
+++ ClassMethods/ClassMethods/Operation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethods
+{
+    public static class Operation
+    {
+        public void outMethod(int num1)
+        {
+            int quotient = num1 / 2;
+            Console.WriteLine(num1 + " divided by 2 equals " + quotient);
+        }
+
+        public void outMethod(int num2, out int result1, out int result2)
+        {
+            result1 = num2 * 10;
+            result2 = result1 / 2;
+            Console.WriteLine(num2 + " multiplied by 10 equals " + result1);
+            Console.WriteLine(result1 + " divided by 2 equals " + result2);
+        }
+    }
+}
---
+++ ClassMethods/ClassMethods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethods
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate class
+            Operation operation = new Operation();
+
+            //// Method 1
+            //// User input
+            //Console.WriteLine("Please enter a number that will be divided by 2.");
+            //int userNum = int.Parse(Console.ReadLine());
+
+            //operation.outMethod(userNum);
+            //Console.ReadLine();
+
+            // Method with output parameters
+            // User input
+            Console.WriteLine("Please enter a number that will be multiplied by 10, then divided by 2");
+            string userString = Console.ReadLine();
+            Int32.TryParse(userString, out int userNum2);
+
+            operation.outMethod(userNum2, out int result1, out int result2);
+            Console.ReadLine();
+        }
+    }
+}
