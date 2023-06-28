b62c986e2046480a93a8e9e52bfabe104b0435db
Joshua Dishong <dishong.joshua@yahoo.com>
2023-06-28 09:01:41 -0400

    updated

--- VoidMethod/VoidMethod/Program.cs
+++ VoidMethod/VoidMethod/Program.cs
@@ -15,10 +15,10 @@ namespace VoidMethod
             Console.WriteLine("Enter a number to be multiplied by 10.");
             int numOne = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter a number to be displayed.");
-            int numTwo = Convert.ToInt32(Console.ReadLine()); 
+            int numTwo = Convert.ToInt32(Console.ReadLine());
 
-            
 
+            operations.voidMethod(50, 20);
             operations.voidMethod(paraOne: numOne, paraTwo: numTwo);
             Console.ReadLine();
         }
