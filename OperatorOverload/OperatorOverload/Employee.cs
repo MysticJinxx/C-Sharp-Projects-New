0d21d05e42b56e0ca1a8275ddb48ea9cb46b4070
Joshua Dishong <dishong.joshua@yahoo.com>
2023-06-30 10:01:16 -0400

    added OperatorOverload

---
+++ OperatorOverload/OperatorOverload/Employee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverload
+{
+    class Employee
+    {
+        public List<Employee> Employees { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            if ((object)employee1 == null)
+                return (object)employee2 == null;
+            return employee1.Equals(employee2);
+        }
+
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return !(employee1 == employee2);
+        }
+
+        //public static bool operator ==(Employee employee1, Employee employee2)
+        //{
+        //    if (employee1.Id == employee2.Id)
+        //        return employee1.Id == employee2.Id;
+        //    else
+        //        return employee1.Id != employee2.Id;
+        //}
+
+        //public static bool operator !=(Employee employee1, Employee employee2)
+        //{
+        //    return employee1.Id != employee2.Id;
+        //}
+
+
+        public override bool Equals(Object obj)
+        {
+            var employee = obj as Employee;
+
+            if (employee == null)
+                return false;
+
+            return this.Id.Equals(employee.Id);
+        }
+    }
+}
---
+++ OperatorOverload/OperatorOverload/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee() { Id = 01, Name = "Joshua" };
+            Employee employee2 = new Employee() { Id = 02, Name = "Gavin" };
+
+            Employee.Equals(employee1, employee2);
+            Console.ReadLine();
+        }
+    }
+}
