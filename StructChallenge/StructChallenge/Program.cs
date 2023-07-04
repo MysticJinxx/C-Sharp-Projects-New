78886e80f84218a1538ea0a25e6f6f87c6761ae7
Joshua Dishong <dishong.joshua@yahoo.com>
2023-07-04 11:29:23 -0400

    added StructChallenge
    practice for structs

---
+++ StructChallenge/StructChallenge/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Struct myStruct = new Struct();
+            myStruct.Amount = 2000 / 10;
+
+            Console.WriteLine(myStruct.Amount);
+            Console.ReadLine();
+        }
+    }
+}
