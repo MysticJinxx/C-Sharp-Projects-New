6405a6b83f699fa9a56be15071f17d49761b90ce
Joshua Dishong <dishong.joshua@yahoo.com>
2023-07-06 11:00:49 -0400

    Added DateTimeConsoleApp
    Simple program created to learn about DateTime

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DateTimeConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Current Date/Time Is: " + DateTime.Now);
+            Console.WriteLine("Please enter a number.");
+            double userNum = double.Parse(Console.ReadLine());
+            Console.WriteLine("{0} + {1} hour(s) = {2}", DateTime.Now, userNum, DateTime.Now.AddHours(userNum));
+
+            Console.ReadLine();
+        }
+    }
+}
